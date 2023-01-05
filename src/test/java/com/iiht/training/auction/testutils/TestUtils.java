7ad39823c53f6fa280b4aea78624283f44bc363f
GitAutopoc <98872904+GitAutopoc@users.noreply.github.com>
2023-01-05 16:57:50 +0530

    Update FuncUrl

--- src/test/java/com/iiht/training/auction/testutils/TestUtils.java
+++ src/test/java/com/iiht/training/auction/testutils/TestUtils.java
@@ -41,7 +41,7 @@ public class TestUtils {
 
 	public static final String GUID = "6ed39465-d6d3-4ec4-b27d-1dcb870b2992";
 	public static String customData;
-	public static final String URL = "https://yaksha-uat-sbfn.azurewebsites.net/api/YakshaMFAEnqueue?code=b2Nouy31ANrObyhFMwy13abG7P7w9I5ls1XeZcXVBSqlE8VDwzBCRA==";
+	public static final String URL = "";
 	static {
 		total = 0;
 		passed = 0;
